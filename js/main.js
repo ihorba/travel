6cf128929643056f8f827f212aa9e39edd0f6d56
ihorba <ihor@footballaddicts.com>
2017-12-23 16:00:24 +0200

    first

---
+++ js/main.js
@@ -0,0 +1,210 @@
+;(function($){
+	"use strict"
+
+	var teamSlider = $('.ba-slider');
+	teamSlider.slick({
+		slide: '.ba-slide',
+		arrows: true,
+		prevArrow: '.ba-slider-prev',
+		nextArrow: '.ba-slider-next',
+		slidesToShow: 1,
+		slidesToScroll:1
+	});
+
+
+	function initMap() {
+		var roma = {lat: 42.462473, lng: 11.832275};
+		var madrid = {lat: 40.416775, lng: -3.703790};
+		var lyon = {lat: 45.764043, lng: 4.835659};
+
+
+		var mapDiv = document.getElementById('ba-map');
+
+		var map = new google.maps.Map(document.getElementById('ba-map'), {
+			zoom: 6,
+			center: roma,
+			disableDefaultUI: true,
+			styles: [
+			{
+				"elementType": "geometry",
+				"stylers": [
+				{
+					"color": "#f5f5f5"
+				}
+				]
+			},
+			{
+				"elementType": "labels.icon",
+				"stylers": [
+				{
+					"visibility": "off"
+				}
+				]
+			},
+			{
+				"elementType": "labels.text.fill",
+				"stylers": [
+				{
+					"color": "#616161"
+				}
+				]
+			},
+			{
+				"elementType": "labels.text.stroke",
+				"stylers": [
+				{
+					"color": "#f5f5f5"
+				}
+				]
+			},
+			{
+				"featureType": "administrative.land_parcel",
+				"elementType": "labels.text.fill",
+				"stylers": [
+				{
+					"color": "#bdbdbd"
+				}
+				]
+			},
+			{
+				"featureType": "poi",
+				"elementType": "geometry",
+				"stylers": [
+				{
+					"color": "#eeeeee"
+				}
+				]
+			},
+			{
+				"featureType": "poi",
+				"elementType": "labels.text.fill",
+				"stylers": [
+				{
+					"color": "#757575"
+				}
+				]
+			},
+			{
+				"featureType": "poi.park",
+				"elementType": "geometry",
+				"stylers": [
+				{
+					"color": "#e5e5e5"
+				}
+				]
+			},
+			{
+				"featureType": "poi.park",
+				"elementType": "labels.text.fill",
+				"stylers": [
+				{
+					"color": "#9e9e9e"
+				}
+				]
+			},
+			{
+				"featureType": "road",
+				"elementType": "geometry",
+				"stylers": [
+				{
+					"color": "#ffffff"
+				}
+				]
+			},
+			{
+				"featureType": "road.arterial",
+				"elementType": "labels.text.fill",
+				"stylers": [
+				{
+					"color": "#757575"
+				}
+				]
+			},
+			{
+				"featureType": "road.highway",
+				"elementType": "geometry",
+				"stylers": [
+				{
+					"color": "#dadada"
+				}
+				]
+			},
+			{
+				"featureType": "road.highway",
+				"elementType": "labels.text.fill",
+				"stylers": [
+				{
+					"color": "#616161"
+				}
+				]
+			},
+			{
+				"featureType": "road.local",
+				"elementType": "labels.text.fill",
+				"stylers": [
+				{
+					"color": "#9e9e9e"
+				}
+				]
+			},
+			{
+				"featureType": "transit.line",
+				"elementType": "geometry",
+				"stylers": [
+				{
+					"color": "#e5e5e5"
+				}
+				]
+			},
+			{
+				"featureType": "transit.station",
+				"elementType": "geometry",
+				"stylers": [
+				{
+					"color": "#eeeeee"
+				}
+				]
+			},
+			{
+				"featureType": "water",
+				"elementType": "geometry",
+				"stylers": [
+				{
+					"color": "#c9c9c9"
+				}
+				]
+			},
+			{
+				"featureType": "water",
+				"elementType": "labels.text.fill",
+				"stylers": [
+				{
+					"color": "#9e9e9e"
+				}
+				]
+			}
+			]
+		});
+
+		var marker1 = new google.maps.Marker({
+			position: roma,
+			map: map,
+			icon: 'img/marker.svg',
+		});
+
+		var marker2 = new google.maps.Marker({
+			position: lyon,
+			map: map,
+			icon: 'img/marker.svg',
+		});
+
+		var marker3 = new google.maps.Marker({
+			position: madrid,
+			map: map,
+			icon: 'img/marker.svg',
+		});
+	}
+
+	window.onload = initMap;
+
+})(jQuery);
